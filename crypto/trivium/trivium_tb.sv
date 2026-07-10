1ea895c011fc5f3faaf2db27bb97a555fdb71f63
ABHILASH MADDINENI <214151262+MrAbhi19@users.noreply.github.com>
2026-07-10 08:25:16 +0530

    Update trivium_tb.sv

--- crypto/trivium/trivium_tb.sv
+++ crypto/trivium/trivium_tb.sv
@@ -1,6 +1,6 @@
 //==============================================================================
 // Copyright (c) 2026 opensiliconhub contributors
-// SPDX-License-Identifier : SHL-2.0
+// SPDX-License-Identifier : Apache-2.0
 //==============================================================================
 
 `timescale 1ns / 1ps
